a34203bb1388614da496aefe0c8a92d2a60ec96d
Richard Ortega <richardjortega@gmail.com>
2018-05-31 13:24:44 -0700

    updated to v3, added info on ingress

---
+++ configure_dns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Public IP address
+IP=$1
+
+# Name to associate with public IP address
+DNSNAME=$2
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with dns name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
--- nodejs-hello-world/server.js
+++ nodejs-hello-world/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, Argentina!');
+  res.send('Hello, Argentina! Hope the weather is better than Seattle!');
 });
 
 app.listen(PORT, HOST);
